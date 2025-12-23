cf8e83841afaa0237160c411dd7d108447e6e460
hyperdb <hyperdb@gmail.com>
2025-12-23 15:23:26 +0900

    ✨ astro.config.mjsにastro-iconをインポートし、integrationsに追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from "astro/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-	output: "static",
-	vite: {
-		plugins: [tsconfigPaths()],
+  output: "static",
+
+  vite: {
+      plugins: [tsconfigPaths()],
 	},
-	site: "https://hyperdb.github.io",
+
+  site: "https://hyperdb.github.io",
+  integrations: [icon()],
 });
